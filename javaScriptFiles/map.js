b3518b353e2a9aed3efc6e9af657a74a4a5b1bf3
Jason <jason.barrett10@gmail.com>
2021-01-06 00:21:27 -0600

    start of google map app6

---
+++ javaScriptFiles/map.js
@@ -0,0 +1,18 @@
+
+    
+    function initMap() { 
+        var options = {
+            zoom:12,
+            center: {lat:43.0731,lng:-89.4012}
+        }
+
+        var map = new google.maps.Map(document.getElementById('map'), options);
+
+
+
+
+
+    }
+
+
+
