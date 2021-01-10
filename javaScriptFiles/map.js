14856f45dacc65baaeb44cd8509611965ee32799
Jason <jason.barrett10@gmail.com>
2021-01-10 00:36:18 -0600

    map with marker and info box working

--- javaScriptFiles/map.js
+++ javaScriptFiles/map.js
@@ -14,25 +14,69 @@
         var map = new google.maps.Map(document.getElementById('map'), options);
 
 
-        // add marker
-        var marker = new google.maps.Marker({
-            position: mid,
-            map: map,   
-            icon: 'http://maps.google.com/mapfiles/kml/pushpin/purple-pushpin.png'
-          });
+        // // add marker
+        // var marker = new google.maps.Marker({
+        //     position: mid,
+        //     map: map,   
+        //     icon: 'http://maps.google.com/mapfiles/kml/pushpin/purple-pushpin.png'
+        //   });
         
 
         
-          var infowindow = new google.maps.InfoWindow({
-          content:  '<p>Middleton WI</p>'
-        });
+        //   var infowindow = new google.maps.InfoWindow({
+        //   content:  '<p>Middleton WI</p>'
+        // });
       
-        marker.addListener("click", function() {
-            infowindow.open(map, marker);
-          });
+        // marker.addListener("click", function() {
+        //     infowindow.open(map, marker);
+        //   });
+
+
+        addMarker({
+          coords:{ lat: 43.0731, lng: -89.4012  },
+          iconImage: 'http://maps.google.com/mapfiles/kml/pushpin/purple-pushpin.png',
+          content:  '<p>Madison WI</p>'
+        });
+        addMarker(
+          {coords:{ lat: 43.0972, lng: -89.5043  }, 
+          iconImage: 'http://maps.google.com/mapfiles/kml/shapes/schools.png',
+          content: '<p>Middleton WI</p>'
+
+        });
+        addMarker(
+          {coords:{ lat: 42.9908, lng: -89.5332  },
+          content: '<p>Verona WI</p>'
+        });
         
 
+        // add marker function 
+        function addMarker(props) {
+        var marker = new google.maps.Marker({
+            position: props.coords,
+            map: map,   
+            //icon: props.iconImage
+          });
+          
+          //check for custom icon
+          if(props.iconImage) {
+            // set icon image
+            marker.setIcon(props.iconImage);
+          }
 
+          //check content 
+          if(props.content) {
+              var infowindow = new google.maps.InfoWindow({
+              content: props.content
+            });
+      
+            marker.addListener("click", function() {
+                infowindow.open(map, marker);
+              });
+            }
+
+
+
+        }
 
 
 
